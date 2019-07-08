dcdde3ce1fb2618db9ed383bb85a07246346835e
Alexey Safin <alexey.r.safin@outlook.com>
2019-07-08 22:57:39 +0500

    1 Среднее задание

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -17,21 +17,73 @@ import shutil
 #     dir_path = os.path.join(os.getcwd(), f'dir_{i+1}')
 #     try:
 #         os.rmdir(dir_path)
-#     except FileExistsError:
+#     except FileNotFoundError:
 #         print('Такой директории нет')
 
 
+def mk_dir(dir_name):
+    dir_path = os.path.join(os.getcwd(), dir_name)
+    try:
+        os.mkdir(dir_path)
+
+    except FileExistsError:
+        print('Такая директория уже существует')
+    else:
+        print(f'Директория {dir_path} создана')
+
+def rm_dir(dir_name):
+    dir_path = os.path.join(os.getcwd(), dir_name)
+    try:
+        os.rmdir(dir_path)
+
+    except FileNotFoundError:
+        print('Такой директории нет')
+    else:
+        print(f'Директория {dir_path} удалена')
+def ch_dir(dir_name):
+    dir_path = os.path.join(os.getcwd(), dir_name)
+    try:
+         os.chdir(dir_path)
+    except FileNotFoundError:
+         print('Такой директории нет')
+    else:
+        print(f'Выбрана директория {dir_path}')
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
 
-# files = os.listdir(path=os.getcwd())
-# for itm in files:
-#     if os.path.isdir(itm):
-#         print(itm)
+def show_dirs():
+    files = os.listdir(path=os.getcwd())
+    if not files:
+        print('Директория пуста')
+    else:
+        for itm in files:
+            if os.path.isdir(itm):
+                print(itm)
+
+def show_files():
+    files = os.listdir(path=os.getcwd())
+    if not files:
+        print('Директория пуста')
+    else:
+        for itm in files:
+            if os.path.isfile(itm):
+                print(itm)
+
+def show_all():
+    files = os.listdir(path=os.getcwd())
+    if not files:
+        print('Директория пуста')
+    else:
+        for itm in files:
+            print(itm)
+
 
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
-name = os.path.basename(__file__)
-copy_filename = f'copy_of_{name}'
-copy_path = os.path.join(os.getcwd(), copy_filename)
-shutil.copyfile(sys.argv[0],copy_path)
+# name = os.path.basename(__file__)
+# copy_filename = f'copy_of_{name}'
+# copy_path = os.path.join(os.getcwd(), copy_filename)
+# shutil.copyfile(sys.argv[0],copy_path)
+
+if __name__ == "__main__":
+    pass
